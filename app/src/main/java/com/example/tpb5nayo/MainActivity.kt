d5a8d248f37e82ed27b9769416a0f2ff1b1b0200
chhue <chhue96@gamil.com>
2023-05-07 23:10:16 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tpb5nayo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tpb5nayo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
