e39ceedf31a3c68f1fe512ac94a9c80369076c04
chhue <chhue96@gmail.com>
2023-05-08 17:50:51 +0900

    xml 레이아웃 구조

--- app/src/main/java/com/example/tpb5nayo/MainActivity.kt
+++ app/src/main/java/com/example/tpb5nayo/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.tpb5nayo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.example.tpb5nayo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.share -> {
+                Toast.makeText(this, "공유하기", Toast.LENGTH_SHORT).show()}
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
