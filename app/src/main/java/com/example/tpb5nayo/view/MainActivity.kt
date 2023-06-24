1bf41ef50e791d21fa0dbc35b2cd636ed215309e
chhue <chhue96@gamil.com>
2023-06-24 22:21:17 +0900

    binding

--- app/src/main/java/com/example/tpb5nayo/view/MainActivity.kt
+++ app/src/main/java/com/example/tpb5nayo/view/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.tpb5nayo.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.tpb5nayo.R
+import com.example.tpb5nayo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
     }
 }
