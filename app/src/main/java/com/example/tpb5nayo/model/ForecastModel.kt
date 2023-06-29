d9e7779ee605dac9a4766f3aafb4fc381d6b6317
chhue <chhue96@gamil.com>
2023-06-29 13:54:44 +0900

    data model

---
+++ app/src/main/java/com/example/tpb5nayo/model/ForecastModel.kt
@@ -0,0 +1,25 @@
+package com.example.tpb5nayo.model
+
+import com.google.gson.annotations.SerializedName
+
+data class ForecastModel (
+    @SerializedName("cod")
+    var cod: String?,
+
+    @SerializedName("cnt")
+    var cnt: Int?,
+
+    @SerializedName("list")
+    var list: List<ForecastListModel>?,
+)
+
+data class ForecastListModel(
+    @SerializedName("dt_txt")
+    var dt_txt: String?,  //2021-08-26 15:00:00
+
+    @SerializedName("main")
+    var main: WeatherMainModel?,
+
+    @SerializedName("weather")
+    var weather: List<WeatherDetailModel>?,
+)
