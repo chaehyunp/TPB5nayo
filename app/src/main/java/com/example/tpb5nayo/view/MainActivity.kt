d756f470c3b63b7484ce08dddfbaac9b135a4f18
chhue <chhue96@gamil.com>
2023-07-03 22:48:58 +0900

    Main

--- app/src/main/java/com/example/tpb5nayo/view/MainActivity.kt
+++ app/src/main/java/com/example/tpb5nayo/view/MainActivity.kt
@@ -2,15 +2,27 @@ package com.example.tpb5nayo.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.databinding.DataBindingUtil
 import com.example.tpb5nayo.R
 import com.example.tpb5nayo.databinding.ActivityMainBinding
+import com.example.tpb5nayo.viewModel.WeatherViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG: String = MainActivity::class.java.simpleName
+    private var viewModel: WeatherViewModel = WeatherViewModel()
+    private lateinit var fabOpen: Animation
+    private lateinit var fabClose: Animation
+    private var isFabOpen: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
     }
+
+    /* View 설정 */
+
 }
